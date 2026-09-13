0adac604a49edbd4da6b2ba4934a402e71759a0b
showxu <10173746+showxu@users.noreply.github.com>
2026-09-14 01:45:32 +0800

    Align CLI build number with App 33

--- Sources/ComputerMCP/ComputerMCPCLI.swift
+++ Sources/ComputerMCP/ComputerMCPCLI.swift
@@ -6,7 +6,7 @@ package enum ComputerMCPCLI {
   package static let version = "1.1.2"
 
   /// Current package build number.
-  package static let build = "32"
+  package static let build = "33"
 
   /// Version string exposed by the command-line executable.
   package static let releaseVersion = "\(version) (\(build))"
